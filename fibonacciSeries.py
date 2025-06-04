29741917a9d2fbea70f886f1e391880efe4f0d35
HamiltonToro <htoroparra@gmail.com>
2025-06-04 14:41:14 -0500

    Fibonacci Series with Python

---
+++ fibonacciSeries.py
@@ -0,0 +1,11 @@
+num1=0
+num2=1
+**Fibonacci=int(input("Digite el número a calcular: "))
+print()
+for i in range(num2,Fibonacci):
+Fibonacci == 0
+	print("La secuencia Fibonacci es: ",num1)
+elif Fibonacci == 1 
+	print("La secuencia Fibonacci es: ",num1,num2,num3)
+print()
+print("Fin")
